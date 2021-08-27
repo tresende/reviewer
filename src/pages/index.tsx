f7e6bc5ebd54b454eb809a6268c61a4849f380ac
Thiago Resende <thiago.gcresende@gmail.com>
2021-08-27 16:38:54 -0300

    add test stack

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,6 @@
-import Home, { HomeProps } from 'templates/Home'
+import Home from 'templates/Home'
+import Mock from 'mocks/mock'
 
-export default function Index(props: HomeProps) {
-  return <Home {...props} />
-}
-
-export async function getStaticProps() {
-  let data = {
-    comments: []
-  }
-  try {
-    const response = await fetch(`${process.env.VERCEL_URL}/api/review`)
-    data = await response.json()
-  } catch {
-    console.warn('Api is down!')
-  }
-  return {
-    revalidate: 60,
-    props: { data }
-  }
+export default function Index() {
+  return <Home data={Mock} />
 }
