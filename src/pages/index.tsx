4919ad2f2b1e6f6bbedf95c8e09e702aeb3c6782
Thiago Resende <thiago.gcresende@gmail.com>
2021-08-27 16:10:42 -0300

    :bug: add review api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,14 @@
-import Home from 'templates/Home'
-import Mock from 'mocks/mock'
+import Home, { HomeProps } from 'templates/Home'
 
-export default function Index() {
-  return <Home data={Mock} />
+export default function Index(props: HomeProps) {
+  return <Home {...props} />
+}
+
+export async function getStaticProps() {
+  const response = await fetch(`${process.env.VERCEL_URL}/api/review`)
+  const data = await response.json()
+  return {
+    revalidate: 60 * 60,
+    props: { data }
+  }
 }
--- src/utilts/date.ts
+++ src/utilts/date.ts
@@ -1,4 +1,6 @@
-export const toPTBRFormat = (date: Date) => {
-  const formatedDate = new Intl.DateTimeFormat('pt-br').format(date)
+export const toPTBRFormat = (date: Date | string) => {
+  let toFormat = date
+  if (typeof date === 'string') toFormat = new Date(date)
+  const formatedDate = new Intl.DateTimeFormat('pt-br').format(toFormat)
   return formatedDate
 }
