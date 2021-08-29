4a6960cce0cb0b8cc89310deb676b995ffd6bdfc
Thiago Resende <thiago.gcresende@gmail.com>
2021-08-29 18:05:55 -0300

    ⬆️  update dependencies

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: 'https://borc-9faa2-default-rtdb.firebaseio.com'
+  API_URL: 'https://borc-9faa2-default-rtdb.firebaseio.com/review.json' //move to .env
 }
 
 export default config
