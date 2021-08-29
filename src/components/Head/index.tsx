54d4ef3a622abd738b135b4da46ffee17ce6ddca
Thiago Resende <thiago.gcresende@gmail.com>
2021-08-29 15:52:08 -0300

    ♿️  Fix ios automaatic zoom

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -4,18 +4,16 @@ const HeadComponent = () => (
   <Head>
     <title>Avaliações de BSB Coworking - Asa Sul - Brasília/DF - BeerOrCoffee</title>
     <link rel="icon" href="/favicon.png" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <meta name="author" content="Thiago Resende" />
     <meta
       name="keywords"
       content="coworking,space,escritório compartilhado,salas,Wi-Fi, Área de alimentação, Ar condicionado, Armário locker compartilhado, Cabine de call, Bicicletário, Café, Chá, Cozinha compartilhada, Dog friendly, Pet friendly, Recepção, Acessibilidade nas áreas comuns"
     />
-
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link rel="shortcut icon" href="/img/icon-512.png" />
     <link rel="apple-touch-icon" href="/img/icon-512.png" />
     <link rel="manifest" href="/manifest.json" />
-
     <meta name="description" content="Conectando pessoas, negócios e propósitos." />
   </Head>
 )
