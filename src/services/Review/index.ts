d42d30d49a0f0c2fa0e0ea9350de081050df22c4
Thiago Resende <thiago.gcresende@gmail.com>
2021-09-01 15:07:02 -0300

    change backend base url

--- src/services/Review/index.ts
+++ src/services/Review/index.ts
@@ -3,7 +3,7 @@ import { timeout } from 'utils/timer'
 
 const save = async (data: UserReview) => {
   await timeout(1000) //fake slow api
-  await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/review`, {
+  await fetch(`${process.env.NEXT_PUBLIC_API_URL}/review.json`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
--- src/services/Review/test.ts
+++ src/services/Review/test.ts
@@ -5,7 +5,7 @@ describe('Sreview Service', () => {
   it('should execute request when function is called', async () => {
     const mock = jest.fn()
     window.fetch = mock
-    process.env.NEXT_PUBLIC_API_URL = 'https://thiagoresende.dev'
+    process.env.NEXT_PUBLIC_API_URL = 'https://borc-9faa2-default-rtdb.firebaseio.com'
 
     const data: UserReview = {
       score: 1,
@@ -14,7 +14,7 @@ describe('Sreview Service', () => {
     }
     await save(data)
     expect(mock).toBeCalledTimes(1)
-    expect(mock).toBeCalledWith('https://thiagoresende.dev/api/review', {
+    expect(mock).toBeCalledWith('https://borc-9faa2-default-rtdb.firebaseio.com/review.json', {
       body: '{"score":1,"text":"text","twitter":false}',
       headers: { 'Content-Type': 'application/json' },
       method: 'POST'
